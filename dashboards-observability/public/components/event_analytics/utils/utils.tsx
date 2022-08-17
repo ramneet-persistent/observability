59721a296f3c741b305fafb2a3ba57655bb62ade
Ramneet Chopra <ramneet_chopra@persistent.com>
2022-08-17 10:59:16 +0530

    feedback
    Signed-off-by: Ramneet Chopra <ramneet_chopra@persistent.com>

--- dashboards-observability/public/components/event_analytics/utils/utils.tsx
+++ dashboards-observability/public/components/event_analytics/utils/utils.tsx
@@ -358,5 +358,4 @@ export const fetchConfigObject = (editor: string, propsOptions: any) => {
 export const filterDataConfigParameter = (parameter: ConfigListEntry[]) =>
   parameter.filter((configItem: ConfigListEntry) => configItem.label);
 
-export const getRoundOf = (value: number, places: number) =>
-  (Math.round(value * 10 ** places) / 10 ** places).toFixed(places);
+export const getRoundOf = (value: number, places: number) => value.toFixed(places);
